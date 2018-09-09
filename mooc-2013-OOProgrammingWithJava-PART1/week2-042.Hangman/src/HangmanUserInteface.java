67d458fc36b514f813756cfe27a788f3ca63ce06
Denislav Georgiev <d1xt1r@gmail.com>
2018-09-09 10:07:44 +0300

    week2-042.Hangman

---
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-042.Hangman/src/HangmanUserInteface.java
@@ -0,0 +1,54 @@
+
+import hangman.Hangman;
+import java.util.Scanner;
+
+public class HangmanUserInteface {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        Hangman hangman = new Hangman();
+
+        System.out.println("***********");
+        System.out.println("* HANGMAN  *");
+        System.out.println("***********");
+        System.out.println("");
+        printMenu();
+        System.out.println("");
+
+        // PROGRAM YOUR SOLUTION HERE
+        System.out.println("Type a command:");
+
+        while (hangman.gameOn()){
+            String command = reader.nextLine();
+            if(command.equals("quit")){
+                break;
+            }
+            else if (command.equals("status")){
+                hangman.printStatus();
+                System.out.println("Type a command:");
+            }
+            else if (command.length() == 1){
+                hangman.guess(command);
+                System.out.println("Type a command:");
+            }
+            else if (command.isEmpty()){
+                System.out.println("String was empty");
+                printMenu();
+            }
+            else {
+                System.out.println("Type a command:");
+            }
+            hangman.printMan();
+            hangman.printWord();
+        }
+        System.out.println("Thank you for playing!");
+    }
+
+    public static void printMenu() {
+        System.out.println(" * menu *");
+        System.out.println("quit   - quits the game");
+        System.out.println("status  - prints the game status");
+        System.out.println("a single letter uses the letter as a guess");
+        System.out.println("an empty line prints this menu");
+    }
+}
