1ada390943e9ff6e5580d6eef2a370886f115afd
Denislav Georgiev <d1xt1r@gmail.com>
2018-09-10 15:16:23 +0300

    week3-052.ReversingName
    Solution will be added after I figure it out

---
+++ mooc-2013-OOProgrammingWithJava-PART1/week3-052.ReversingName/src/ReversingName.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReversingName {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type your name: ");
+        String name = reader.nextLine();
+
+        // solution will be added after I figure it out
+        }
+    }
+}
