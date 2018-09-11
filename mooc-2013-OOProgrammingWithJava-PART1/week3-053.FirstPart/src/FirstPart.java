34102c9629f455749d2a36635c7d1cdecfc97051
Denislav Georgiev <d1xt1r@gmail.com>
2018-09-11 12:30:51 +0300

    week3-053.FirstPart

---
+++ mooc-2013-OOProgrammingWithJava-PART1/week3-053.FirstPart/src/FirstPart.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class FirstPart {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type a word: ");
+        String word = reader.nextLine();
+
+        System.out.println("Length of the first part: ");
+        Integer length = Integer.parseInt(reader.nextLine());
+
+        String firstPart = word.substring(0,length);
+
+        System.out.println("Result: " + firstPart);
+    }
+}
