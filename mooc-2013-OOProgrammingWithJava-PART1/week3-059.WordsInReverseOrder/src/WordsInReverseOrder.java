6b64804c257c64975c4f3325e54ae6ae7522a76b
d1xt1r <d1xt1r@gmail.com>
2018-11-07 13:26:54 +0200

    New solution for week3-059.WordsInReverseOrder

--- mooc-2013-OOProgrammingWithJava-PART1/week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -5,23 +5,22 @@ import java.util.Scanner;
 public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        // create here an ArrayList
         ArrayList<String> words = new ArrayList<String>();
 
-        while (true) {
+        System.out.println("Type a word: ");
+        String userInput = reader.nextLine();
 
+        while (!userInput.isEmpty()){
+            words.add(userInput);
             System.out.println("Type a word: ");
-            String word = reader.nextLine();
-            words.add(word);
-
-            if (word.isEmpty()) {
-                Collections.reverse(words);
-                System.out.println("You typed the following words: " + words);
-                break;
-            }
+            userInput = reader.nextLine();
         }
-        for (String print : words) {
-            System.out.println(print);
+        Collections.reverse(words);
+        System.out.println("You typed the following words: ");
+
+        for(String word : words){
+            System.out.println(word);
         }
     }
 }
