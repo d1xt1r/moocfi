29b116a7b8d4bf0edc3a48a7308d6ac21525f4f8
d1xt1r <d1xt1r@gmail.com>
2018-11-07 12:01:35 +0200

    New solution for week3-058.RecurringWord

--- mooc-2013-OOProgrammingWithJava-PART1/week3-058.RecurringWord/src/RecurringWord.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week3-058.RecurringWord/src/RecurringWord.java
@@ -1,22 +1,26 @@
 
 import java.util.ArrayList;
-import java.util.Scanner;
+        import java.util.Scanner;
 
 public class RecurringWord {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        // create here the ArrayList
 
         ArrayList<String> words = new ArrayList<String>();
 
+        System.out.println("Type a word: ");
+        String userWord = reader.nextLine();
+
         while (true){
+            words.add(userWord);
             System.out.println("Type a word: ");
-            String word = reader.nextLine();
-            if(words.contains(word)){
-                System.out.println("You gave the word " + word + " twice.");
+            userWord = reader.nextLine();
+            if(words.contains(userWord)){
+                System.out.println("You gave the word " + userWord + " twice");
                 break;
             }
-            words.add(word);
         }
     }
 }
