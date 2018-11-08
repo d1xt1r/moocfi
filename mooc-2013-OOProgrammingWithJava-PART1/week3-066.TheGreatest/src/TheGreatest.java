74c6fbc76f8d5b3ecb93675dc734ad3f3c2c2d12
d1xt1r <d1xt1r@gmail.com>
2018-11-08 12:34:06 +0200

    New solution for week3-066.TheGreatest

--- mooc-2013-OOProgrammingWithJava-PART1/week3-066.TheGreatest/src/TheGreatest.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week3-066.TheGreatest/src/TheGreatest.java
@@ -2,15 +2,14 @@ import java.util.ArrayList;
 
 public class TheGreatest {
     public static int greatest(ArrayList<Integer> list) {
-
-        int greatestSoFar  = list.get(0);
-
-        for (int numberFromList : list) {
-            if (numberFromList > greatestSoFar ) {
-                greatestSoFar  = numberFromList;
+        // write code here
+        int i = list.get(0); // get the first in the list and check if it is greatest || it works with i = 0 but it will fail if there are negative numbers in the list.
+        for(int number : list){ // go through the whole list
+            if(i < number){ // if a greater number in the list is found
+                i = number; // the previous one is replaced by the new greatest number.
             }
         }
-        return greatestSoFar;
+        return i;
     }
 
     public static void main(String[] args) {
@@ -19,7 +18,7 @@ public class TheGreatest {
         lista.add(2);
         lista.add(7);
         lista.add(2);
-
+        
         System.out.println("The greatest number is: " + greatest(lista));
     }
 }
